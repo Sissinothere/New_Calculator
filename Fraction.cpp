addfde28b0c9f1a83ee3b3c1440e77e4f3854bcc
JohnnyRacket <john.randolph.harrison@gmail.com>
2014-04-14 22:36:33 -0400

    cleaned frac

--- Fraction.cpp
+++ Fraction.cpp
@@ -6,26 +6,22 @@
 #include <string>
 #include "Fraction.h"
 using namespace std;
-///write about logs in parens
+
 Fraction::Fraction(string sValue){
 	isInt= false;
-	cout << "this is the constructor"<< endl;
 	input = sValue;
 	checkValues();
-	cout << "post check values"<<endl;
+
 	string temp;
 	int i =0;
 
 	cout << sValue.length()<<endl;
 	while(sValue.at(i)!='/'&& !isInt){
 		num += sValue.at(i);
-		cout << "in loop"<<endl;
 		++i;
 	}
-	cout << "this is the string"<< num << endl;
 	num1 = atoi(num.c_str());
 	if(isInt)num1 = atoi(sValue.c_str());
-	cout <<"this is num1 "<< num1<<endl;
 	i++;
 	while(i< sValue.length()){
 			den += sValue.at(i);
@@ -33,68 +29,50 @@ Fraction::Fraction(string sValue){
 		}
 		den1 = atoi(den.c_str());
 		if(isInt)den1=1;
-		cout <<"this is den "<< den1<<endl;
-cout << "right before reduction" <<endl;
 	Reduction();
 	//checkValues();
 	isInt=false;
-cout <<"end of constructor "<< endl;
+
 }
 
 void Fraction::checkValues(){
-	cout << "in check value"<<endl;
-	cout << "this is the string"<< num << endl;
+
 	string temp="";
 	int i=0;
 	isInt=true;
 	for(i; i<input.length();i++){
 		if(input[i]=='/'){
 			isInt=false;
-			cout << "this is false"<<endl;
 		}
 	}
 }
 void Fraction::Reduction(){
 	ostringstream convert;
-	/*if(den1==0){
-		cout<<"cannot solve equation!!!!"<<endl;
-	}
-	for(int i; i<=num1&&i<=den1;i++){
-		if(num1%i==0 && den1%i ==0){
 
-		}
-	}*/
-cout << "in reduction"<< endl;
-cout <<"num1 in reducation"<<num1<< endl;
-cout <<"den1 in reduction" << den1<<endl;
+
 
 	if (num1 < den1){
-		cout << "in if 1"<< endl;
 		for (int i = num1; i >0; i--){
 			cout <<"in for 1 "<< endl;
 			if (num1%i ==0 && den1%i==0){
 				num1 = num1/i;
 				den1 = den1/i;
-				cout << num1;
-				cout << "/";
-				cout << den1;
+
 
 			}
 		}
 	}
-	cout <<" after if num <"<< endl;
+
 	if (num1 > den1){
 		for(int i = den1; i >0; i--){
 			if (num1%i ==0 && den1%i==0){
 			num1 = num1/i;
 			den1 = den1/i;
-			cout << num1;
-			cout <<"/"<<den1;
+
 			}
 		}
 	}
 	if(num1== den1){
-		cout << "!!!!!!!!!!!!!!!!!1"<<endl;
 		isInt = true;
 		answer = "1";
 		num1 = 1;
@@ -126,17 +104,14 @@ void Fraction::Addition(Fraction& frac){
 	int lcd;
 	 if(den1 == 0 || frac.getDenominator() == 0)
 	    {
-	        cout << "Cannot solve equation!!!!" << endl; //use throw catch
-
 	    }
 	   for(int i=1;i<=den1 && i<=frac.getDenominator();i++){
 	      if(den1%i == 0 && frac.getDenominator()%i ==0){
 	    	  	  gcd=i;
 	       }else{}//do nothing;
 	   	}
-	    cout << "______GCD is: " << gcd << endl;
 	     lcd = (den1 * (frac.getDenominator())) / gcd;
-	cout << "lcd is "<<lcd<<endl;
+
 
 	int frac1Mult = lcd/den1;
 	int frac2Mult = lcd/frac.getDenominator();
@@ -148,8 +123,7 @@ void Fraction::Addition(Fraction& frac){
 	den1 = lcd;
 	Reduction();
 
-	cout <<"this is num1 right before ss "<<num1<<endl;
-	cout <<"this si den 1 right before ss "<<den1<<endl;
+
 	stringstream ss;
 	ss<< num1;
 	ss<< "/";
@@ -160,7 +134,7 @@ void Fraction::Addition(Fraction& frac){
 						ss<< num1;
 						answer = ss.str();
 			}
-	cout <<"this is the value stored at answer: "<< answer<<endl;
+
 
 }
 void Fraction::Multiplication(Fraction& frac){
@@ -193,16 +167,13 @@ void Fraction::Division(Fraction& frac){
 							ss<< num1;
 							answer = ss.str();
 				}
-		cout << answer<<endl;
 }
 
 
 int Fraction::getNumerator(){
 	return num1;
 }
-//void Fraction::setNumerator(int a){
-//	num1 = a;
-//}
+
 int Fraction::getDenominator(){
     return den1;
 }
